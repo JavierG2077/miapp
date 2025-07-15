f702a89446b6c1e635be75ca19a7c5409fcc3df8
neyoficial <jgallardoesp@gmail.com>
2025-07-15 13:12:28 -0400

    Seq ID and delete

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const formulario = document.querySelector(".form-create");
+
+/* este funciona como contador para el id pero el profe nos dijo de otra manera
+let id = 1;
+
+function c_id(){
+    return id++;
+}
+    <p>id: ${c_id()}</p> */
+
+    let id = 1;
+
 formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML += `<div class="publicacion">
-            <p>id: 00</p>
+    contenedor.innerHTML += `<div id="${id}" class="publicacion">
+            <p>id: ${id}</p>
             <p>${descripcion}</p>
             <button>editar</button>
-            <button>eliminar</button>
+            <button id="eliminar" onclick="eliminar(this)">eliminar</button>
         </div>`;
+        id++;
 });
+
+function eliminar(e){
+    e.parentElement.remove(this)
+}
